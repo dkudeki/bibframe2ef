6b867b9e53dbb1e466cc3c5674a9d58aaf2635bf
Deren Kudeki <dkudeki@illinois.edu>
2019-10-27 22:32:51 -0500

    Stylesheet portion should also report days in duration report

--- xsl/transformBIBF2JSON.py
+++ xsl/transformBIBF2JSON.py
@@ -21,16 +21,16 @@ def applyXSLStylesheetToBIBFRAMEXML(filename,input_folder,output_folder,saxon_ja
 		print("Finished transforming " + filename)
 
 def transformBIBF2JSON(input_folder,output_folder,saxon_jar,core_count):
-	start_time = datetime.datetime.now().time()
+	start_time = datetime.datetime.now()
 	p = Pool(core_count)
 	for root, dirs, files in os.walk(input_folder):
 		p.map(applyXSLStylesheetToBIBFRAMEXML,iterable=zip([f for f in files if f[-4:] == '.xml'],repeat(input_folder),repeat(output_folder),repeat(saxon_jar)))
 
-	end_time = datetime.datetime.now().time()
+	end_time = datetime.datetime.now()
 	print('XSL TRANSFORM:')
 	print("Start time: " + str(start_time))
 	print("End time: " + str(end_time))
-	print("Run duration: " + str(datetime.datetime.combine(datetime.date.min,end_time)-datetime.datetime.combine(datetime.date.min,start_time)))
+	print("Run duration: " + str(end_time-start_time))
 
 if __name__ == "__main__":
 	transformBIBF2JSON(sys.argv[1],sys.argv[2],sys.argv[3],int(sys.argv[4]))
